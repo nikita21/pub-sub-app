eed16559cdcc39e65f8f918a11c1ccd4b6059e18
nikita.mantri <nikita.mantri21@gmail.com>
2016-10-25 01:37:24 +0530

    executing multiple sensors and multiple subscirbers

--- temperature-simulation/src/main/java/com/pubsub/temperature_simulation/App.java
+++ temperature-simulation/src/main/java/com/pubsub/temperature_simulation/App.java
@@ -8,7 +8,31 @@ public class App
     private static ExecutorService executor = Executors.newFixedThreadPool(10);
     public static void main( String[] args )
     {
-        executor.submit(new TemperatureSensor("sensor-1", "bangalore-temperature"));
+        createPublishers("sensor-1", "BANGALORE_TEMPERATURE");
+        createPublishers("sensor-2", "UDAIPUR_TEMPERATURE");
+        createPublishers("sensor-3", "MUMBAI_TEMPERATURE");
+        createPublishers("sensor-4", "GURGAON_TEMPERATURE");
         
+        createSubscribers("subscriber-1", "BANGALORE_TEMPERATURE");
+        createSubscribers("subscriber-2", "BANGALORE_TEMPERATURE");
+        createSubscribers("subscriber-3", "UDAIPUR_TEMPERATURE");
+        createSubscribers("subscriber-4", "BANGALORE_TEMPERATURE");
+        createSubscribers("subscriber-5", "UDAIPUR_TEMPERATURE");
+        createSubscribers("subscriber-6", "MUMBAI_TEMPERATURE");
+        createSubscribers("subscriber-7", "GURGAON_TEMPERATURE");
+        createSubscribers("subscriber-8", "MUMBAI_TEMPERATURE");
+        createSubscribers("subscriber-9", "UDAIPUR_TEMPERATURE");
+        createSubscribers("subscriber-10", "GURGAON_TEMPERATURE");
+        
+    }
+    
+    private static void createPublishers(String clientId, String topicName)
+    {
+	executor.submit(new TemperatureSensor(clientId, topicName));
+    }
+    
+    private static void createSubscribers(String clientId, String topicName)
+    {
+	executor.submit(new Subscriber(clientId, topicName));
     }
 }
